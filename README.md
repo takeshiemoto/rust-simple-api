d756240a0da3ba438a7dac8f2fe4f3a14f9307ca
Takeshi Emoto <takeshi.emoto@jointcrew.co.jp>
2023-01-09 07:26:39 +0900

    done

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # rust-log-collector
+
+### Usage
+
+```bash
+# POST CSV
+curl -F 'file=@test.csv;type=text/csv' http://localhost:3000/csv
+
+# POST CSV
+curl -F 'file=@test.csv;type=text/csv' http://localhost:3000/csv
+```
--- server/src/handlers.rs
+++ server/src/handlers.rs
@@ -1,13 +1,59 @@
 use crate::model::NewLog;
 use crate::{db, Server};
-use actix_web::{HttpResponse, Json, Query, State};
+use actix_web::{FutureResponse, HttpResponse, Json, Query, State};
+use actix_web_multipart_file::{FormData, Multiparts};
 use chrono::DateTime;
+use csv::Writer;
+use diesel::{PgConnection, QueryDsl};
 use failure::Error;
+use futures::prelude::*;
+use itertools::Itertools;
 use log::debug;
+use std::borrow::Borrow;
+use std::io::{BufReader, Read};
 
-pub fn handle_post_csv(server: State<Server>) -> Result<HttpResponse, Error> {
-    let logs = Default::default();
-    Ok(HttpResponse::Ok().json(api::csv::post::Response(logs)))
+pub fn handle_post_csv(
+    server: State<Server>,
+    multiparts: Multiparts,
+) -> FutureResponse<HttpResponse> {
+    let fut = multiparts
+        .from_err()
+        .filter(|field| field.content_type == "text/csv")
+        .filter_map(|field| match field.form_data {
+            FormData::File { file, .. } => Some(file),
+            FormData::Data { .. } => None,
+        })
+        .and_then(move |file| load_file(&*server.pool.get()?, file))
+        .fold(0, |acc, x| Ok::<_, Error>(acc + x))
+        .map(|sum| HttpResponse::Ok().json(api::csv::post::Response(sum)))
+        .from_err();
+
+    Box::new(fut)
+}
+
+fn load_file(conn: &PgConnection, file: impl Read) -> Result<usize, Error> {
+    use crate::model::NewLog;
+
+    let mut ret = 0;
+
+    let in_csv = BufReader::new(file);
+    let in_log = csv::Reader::from_reader(in_csv).into_deserialize::<::api::Log>();
+
+    for logs in &in_log.chunks(1000) {
+        let logs = logs
+            .filter_map(Result::ok)
+            .map(|log| NewLog {
+                user_agent: log.user_agent,
+                response_time: log.response_time,
+                timestamp: log.timestamp.naive_utc(),
+            })
+            .collect_vec();
+
+        let inserted = db::insert_logs(conn, &logs)?;
+        ret += inserted.len()
+    }
+
+    Ok(ret)
 }
 
 pub fn handle_post_logs(
@@ -55,8 +101,21 @@ pub fn handle_get_csv(
     server: State<Server>,
     range: Query<api::csv::get::Query>,
 ) -> Result<HttpResponse, Error> {
-    debug!("{:?}", range);
-    let csv: Vec<u8> = vec![];
+    use chrono::{DateTime, Utc};
+    let conn = server.pool.get()?;
+    let logs = db::logs(&conn, range.from, range.until)?;
+    let v = Vec::new();
+    let mut w = csv::Writer::from_writer(v);
+
+    for log in logs.into_iter().map(|log| ::api::Log {
+        user_agent: log.user_agent,
+        response_time: log.response_time,
+        timestamp: DateTime::from_utc(log.timestamp, Utc),
+    }) {
+        w.serialize(log)?
+    }
+
+    let csv = w.into_inner()?;
     Ok(HttpResponse::Ok()
         .header("Content-Type", "text/csv")
         .body(csv))
