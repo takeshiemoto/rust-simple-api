dc38708f82fd224c44166c4bd2f4e233258f9316
takeshiemoto <private.takeshiemoto@gmail.com>
2024-02-12 01:02:09 +0900

    feat: add handler

--- src/handlers.rs
+++ src/handlers.rs
@@ -1,5 +1,5 @@
 use crate::repositories::{CreateTodo, TodoRepository};
-use axum::extract::Extension;
+use axum::extract::{Extension, Path};
 use axum::http::StatusCode;
 use axum::response::IntoResponse;
 use axum::Json;
@@ -8,12 +8,39 @@ use std::sync::Arc;
 use std::time::Duration;
 use tokio::time::sleep;
 
+// Extension抽出器
+// アプリケーションの状態や依存関係をハンドラに注入するために使用されます。
+// これにより、共有状態や他のリソースへのアクセスをハンドラ関数内で容易にできるようになります。
+// create_todoでは、Extension<Arc<T>>を使用して、TodoRepositoryのインスタンスをハンドラに注入しています。
+// Json(payload)では、リクエストボディをデシリアライズしてCreateTodo型に変換しています。
 pub async fn create_todo<T: TodoRepository>(
     Json(payload): Json<CreateTodo>,
     Extension(repository): Extension<Arc<T>>,
 ) -> impl IntoResponse {
     let todo = repository.create(payload);
     (StatusCode::CREATED, Json(todo))
+}
+
+pub async fn find_todo<T: TodoRepository>(
+    Path(id): Path<i32>,
+    Extension(repository): Extension<Arc<T>>,
+) -> Result<impl IntoResponse, StatusCode> {
+    Ok(StatusCode::OK)
+}
+
+pub async fn all_todos<T: TodoRepository>(
+    Extension(repository): Extension<Arc<T>>,
+) -> Result<impl IntoResponse, StatusCode> {
+    todo!();
+    Ok(StatusCode::OK)
+}
+
+pub async fn delete_todo<T: TodoRepository>(
+    Path(id): Path<i32>,
+    Extension(repository): Extension<Arc<T>>,
+) -> Result<impl IntoResponse, StatusCode> {
+    todo!();
+    Ok(StatusCode::OK)
 }
 
 pub async fn root() -> &'static str {
